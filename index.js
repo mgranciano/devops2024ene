6edf2be4fb4afd1f21dd0d2096de70c1990459c3
mgranciano <mgranciano@baufest.com>
2024-01-27 12:52:48 -0600

    chore:add hostname

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const os = require('os');
+ 
+// Obtener el nombre de la máquina
+const hostname = os.hostname();
+ 
+// Mostrar el nombre de la máquina
+
+console.log('El nombre de la máquina es:', hostname);
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello new World 2');
+  res.send('El nombre de la máquina es:' + hostname);
 })
 
 module.exports = app;
